24622ff9e5d6e0ebb196ba7fc04def292ea3bc86
nick <nick.holt@btinternet.com>
2023-01-02 12:09:25 +0000

    Adding isNullOrWhitespace method

--- src/main/java/com/tn/lang/Strings.java
+++ src/main/java/com/tn/lang/Strings.java
@@ -4,6 +4,11 @@ public class Strings
 {
   public static final String EMPTY = "";
 
+  public static boolean isNullOrWhitespace(String s)
+  {
+    return s == null || s.trim().length() == 0;
+  }
+
   public static String orElseEmpty(String s, boolean b)
   {
     return b ? s : EMPTY;
